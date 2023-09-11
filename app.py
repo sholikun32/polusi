fe023a47a090079d7f09775439651d4c69ccc83d
sholikun32 <102804771+sholikun32@users.noreply.github.com>
2023-09-11 23:02:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,12 @@ def display_data():
             st.write(df)
 
             # Mengunduh data sebagai file Excel
+            towrite = io.BytesIO()
+            downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, header=True)
+            towrite.seek(0)  # reset pointer
+            b64 = base64.b64encode(towrite.read()).decode()  # some strings
+            linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="myfilename.xlsx">Download excel file</a>'
+            st.markdown(linko, unsafe_allow_html=True)
             
             
         else:
